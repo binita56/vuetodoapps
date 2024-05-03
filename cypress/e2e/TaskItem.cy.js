58c0bdf0e9974e95f1f2a4279132985f35690631
binita shrestha <stha.binita56@gmail.com>
2024-05-03 11:43:25 +0545

    last commit checking

--- cypress/e2e/TaskItem.cy.js
+++ cypress/e2e/TaskItem.cy.js
@@ -1,23 +1,21 @@
 describe("TaskItem Component", () => {
   it("clicks complete button", () => {
-    cy.visit("/"); // Update the URL if necessary
+    cy.visit("/"); 
 
-    // Find all complete buttons and click them one by one
+   
     cy.get(".circle").each(($button) => {
       cy.wrap($button).click();
     });
 
-    // Assert that the 'complete' event is emitted for each button clicked
+  
   });
 
   it("clicks remove button", () => {
-    cy.visit("/"); // Update the URL if necessary
+    cy.visit("/"); 
 
-    // Find all remove buttons and click them one by one
     cy.get(".bi-trash3").each(($button) => {
       cy.wrap($button).click();
     });
 
-    // Assert that the 'remove' event is emitted for each button clicked
   });
 });
--- cypress/e2e/TaskTodo.cy.js
+++ cypress/e2e/TaskTodo.cy.js
@@ -1,9 +1,9 @@
 describe("TaskTodo Component", () => {
   it("renders correctly and interacts with tasks", () => {
-    // Visit the webpage where the TaskTodo component is hosted
-    cy.visit("/"); // Update the URL if necessary
 
-    // Verify that the TaskTodo component exists
+    cy.visit("/"); 
+
+
     cy.get(".container").should("exist");
     cy.get(".task").should("exist");
     cy.get(".title").should("exist").contains("To Do List");
@@ -12,16 +12,14 @@ describe("TaskTodo Component", () => {
     cy.get(".clearBtns").should("exist");
     cy.get(".pendingTasks").should("exist");
 
-    // Interact with the TaskTodo component
+
     cy.get(".form input[type='text']").type("Test Task{enter}");
 
-    // Assert the task is completed
+
     cy.get(".pendingTasks span").should("contain", "Pending Tasks: ");
 
-    // Remove a task
+  
     cy.get(".taskItems ul li:first-child .bi-trash3").click();
 
-    // Assert the task is removed
-    // cy.get(".taskItems ul").should("not.contain", "Test Task");
   });
 });
